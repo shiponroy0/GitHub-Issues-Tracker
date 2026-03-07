e0b974428a64dd48c517f3744cb4783148c0866f
Shipon Roy <shiponworks@gmail.com>
2026-03-07 13:32:45 +0600

    Complete: Modal functionality

--- Js/functionality.js
+++ Js/functionality.js
@@ -186,4 +186,115 @@ const searchIssues = async () => {
   loadingEffect(false);
 };
 
+// Modal Data fetch
+const showModalDataFetch = async id => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  showModalDataDisplay(data.data);
+};
+
+const showModalDataDisplay = data => {
+  console.log(data);
+
+  const priorityColor = {
+    high: 'bg-red-500/10 text-red-500',
+    medium: 'bg-yellow-500/10 text-yellow-500',
+    low: 'bg-[#EEEFF2] text-[#9CA3AF]',
+  };
+
+  modalContent.innerHTML = `
+      
+      
+                <!-- details -->
+          <div class=" space-y-4 w-full">
+
+            <div class="space-y-2">
+              <!-- title -->
+              <div>
+                <h3 class="text-2xl font-bold">${data.title}</h3>
+              </div>
+
+              <!-- status and author -->
+              <div>
+                <ul class="flex items-center gap-2">
+                  <li class="list-none ${
+                    data.status === 'open'
+                      ? 'bg-greenColor text-white'
+                      : 'bg-[#A855F7] text-white'
+                  } text-sm font-medium px-2 py-0.5 rounded-full">${data.status.toUpperCase()}
+                  </li>
+                  <li class="before:content-[''] before:text-paragraphColor before:mr-1">${data.author}</li>
+                  <li class="before:content-[''] before:text-paragraphColor before:mr-1">${new Date(
+                    data.updatedAt,
+                  ).toLocaleDateString()}</li>
+                </ul>
+              </div>
+            </div>
+
+            <!-- bug -->
+            <div class="flex gap-2 items-center">
+
+              <!-- 1 -->
+              <div
+                class="bg-readColor/10 max-w-max px-3 py-1 rounded-full gap-1 flex items-center border border-readColor/30">
+                <img src="../assets/icons/BugDroid.png" alt="icons" class="">
+                <h2 class="text-readColor text-[13px] uppercase">${data?.labels[0]}</h2>
+              </div>
+
+              <!-- help wanted 2-->
+              <div class=" max-w-max px-3 py-1 rounded-full flex items-center border ${
+                data?.labels[1] === 'enhancement'
+                  ? 'bg-[#BBF7D0]/25 border-[#00A96E]/20'
+                  : 'bg-warningLight/80 border-warningColor'
+              }">
+                <img
+                  src="${data?.labels[1] === 'enhancement' ? '../assets/icons/Sparkle.png' : '../assets/icons/Lifebuoy.png'}"
+                  alt="icons">
+                <h2 class="${
+                  data?.labels[1] === 'enhancement'
+                    ? 'text-success bg-[#BBF7D0]'
+                    : 'text-warningColor'
+                } badge text-[13px] uppercase">${data?.labels[1]}</h2>
+              </div>
+
+
+            </div>
+
+            <!-- subtitle -->
+            <div>
+              <p class="text-paragraphColor">${data?.description}</p>
+            </div>
+
+            <!-- Assignee: Priority: -->
+            <div class="flex items-center justify-between bg-[#F8FAFC] px-4 py-2 rounded-lg">
+
+              <!-- Assignee -->
+              <div class="space-y-2">
+                <p class="text-paragraphColor">Assignee:</p>
+                <h2>${data?.assignee}</h2>
+              </div>
+
+              <!-- Priority:  -->
+              <div class="space-y-2">
+                <p class="text-paragraphColor">Priority:</p>
+                <h2 class="${priorityColor[data?.priority]} py-0.5 px-4 rounded-full max-w-max">${data.priority}</h2>
+              </div>
+
+              <div></div>
+
+            </div>
+
+          </div>
+
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button, it will close the modal -->
+              <button class="btn btn-primary outline-none">Close</button>
+            </form>
+          </div>
+      
+      `;
+};
+
 allIssuesDataFetch();
